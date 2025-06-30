e94e522fb2d02005fdd8db4ddb7cb6e99047ceae
Jonathan Flores <jonathan.flores@mail.udp.cl>
2025-06-30 15:51:57 -0400

    refactor: Remove unused CSS and clean up App component imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import {AuthContextProvider, MyRoutes} from "./index"
 
 function App() {
